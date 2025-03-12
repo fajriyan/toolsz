447b60ac477fc9e1358a0bcdfd931e51db77a6d5
fajriyan <fajriyan20@gmail.com>
2025-03-12 09:00:21 +0700

    add google analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import Navbar from "@/components/navbar/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/components/footer/Footer";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -126,6 +127,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <head>
+      <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-L8KVKEM9NS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){ dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', 'G-L8KVKEM9NS');
+          `}
+        </Script>
         <script
             type="application/ld+json"
             dangerouslySetInnerHTML={{ __html: JSON.stringify(metadata.schema) }}
